37d8e24d0f3da2410b9a4aa78fafe5c34530ae3a
chuchulin <chuchulin_99@hotmail.com>
2016-08-18 15:46:19 -0500

    Update ejemplo-4.js

--- rv/ejemplo-4.js
+++ rv/ejemplo-4.js
@@ -9,4 +9,5 @@ var material =new THREE.MeshNormalMaterial();
 var cubo =new THREE.Mesh (forma,material);
 cubo.rotateX(-Math.PI/4);
 cubo.rotateY(Math.PI/4);
+escena.add(cubo);
 renderizador.render(escena,camara)
