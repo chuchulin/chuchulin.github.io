582470dae3c31962a32c04fcc6325ace0dc6576d
chuchulin <chuchulin_99@hotmail.com>
2016-08-18 15:49:30 -0500

    Update ejemplo-4.js

--- rv/ejemplo-4.js
+++ rv/ejemplo-4.js
@@ -1,6 +1,6 @@
 var escena =new THREE.Scene();
 var camara =new THREE.PerpectiveCamera();
-camera.position.z=5;
+camara.position.z=5;
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95),
 document.body.appendChild(renderizador.domElement);
