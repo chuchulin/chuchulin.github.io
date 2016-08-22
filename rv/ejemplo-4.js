74c5fc37ae72626610d2db6845d3d933be00d3e2
chuchulin <chuchulin_99@hotmail.com>
2016-08-21 19:49:28 -0500

    Update ejemplo-4.js

--- rv/ejemplo-4.js
+++ rv/ejemplo-4.js
@@ -5,7 +5,7 @@ var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95),
 document.body.appendChild(renderizador.domElement);
 var forma =new THREE.BoxGeometry (1,1,1);
-var material =new THREE.MeshNormalMaterial();
+var material =new THREE.MeshBasicMaterial();
 var cubo =new THREE.Mesh (forma,material);
 cubo.rotateX(-Math.PI/4);
 cubo.rotateY(Math.PI/4);
