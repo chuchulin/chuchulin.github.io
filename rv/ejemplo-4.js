de049bbda09ddcf138892b00033320d1837dc617
chuchulin <chuchulin_99@hotmail.com>
2016-08-21 19:57:47 -0500

    Update ejemplo-4.js

--- rv/ejemplo-4.js
+++ rv/ejemplo-4.js
@@ -1,13 +1,13 @@
 var escena =new THREE.Scene();
 var camara =new THREE.PerpectiveCamera();
-camara.position.Z=5;
+camara.position.z=5;
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95),
 document.body.appendChild(renderizador.domElement);
 var forma =new THREE.BoxGeometry (1,1,1);
 var material =new THREE.MeshBasicMaterial();
 var cubo =new THREE.Mesh (forma,material);
-cubo.rotateX(-Math.PI/4);
-cubo.rotateY(Math.PI/4);
+//cubo.rotateX(-Math.PI/4);
+//cubo.rotateY(Math.PI/4);
 escena.add(cubo);
 renderizador.render(escena,camara)
