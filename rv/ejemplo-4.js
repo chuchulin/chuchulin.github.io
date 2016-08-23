d038ded7469c7aa17670f6e26ea8f6c65aeae911
chuchulin <chuchulin_99@hotmail.com>
2016-08-23 14:55:50 -0500

    Update ejemplo-4.js

--- rv/ejemplo-4.js
+++ rv/ejemplo-4.js
@@ -3,17 +3,11 @@ var camara = new THREE.PerspectiveCamera();
 camara.position.z = 5;
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95, window.innerHeight*.95);
-// document.body.appendChild -> Añade una rama a body
 document.body.appendChild(renderizador.domElement);
-// Describimos la forma de la figura
 var forma = new THREE.BoxGeometry(1, 1, 1);
-// Asignamos el material a la figura
 var material = new THREE.MeshNormalMaterial();
-// Creamos la figura
 var cubo = new THREE.Mesh(forma, material);
-// Usa radianes
 cubo.rotateX(-Math.PI/4);
 cubo.rotateY(Math.PI/4);
-// Genera la imagen
 escena.add(cubo);
 renderizador.render(escena,camara);
