a985538bf426405597ea123abf86eceec476e829
chuchulin <chuchulin_99@hotmail.com>
2016-08-25 15:13:02 -0500

    Update ejemplo-figura.js

--- rv/ejemplo-figura.js
+++ rv/ejemplo-figura.js
@@ -8,7 +8,7 @@ var malla = new THREE.Mesh(forma);
 var escena = new THREE.Scene();
 escena.add(malla);
 var camara =new THREE.PerspectiveCamera();
-camera.position.z=100;
+camara.position.z=100;
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
