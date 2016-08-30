6127e6ccfeb2419aef15d1a8b9f648f238c47891
chuchulin <chuchulin_99@hotmail.com>
2016-08-30 15:08:58 -0500

    Update arbol-union.js

--- rv/arbol-union.js
+++ rv/arbol-union.js
@@ -14,9 +14,9 @@ var arbolMalla= new THREE.Mesh(arbolForma,material);
 
 var escena = new THREE.Scene();
 escena.add(arbolMalla);
-var camera = new THREE.PerspectiveCamera();
-camera.position.z=5;
+var camara = new THREE.PerspectiveCamera();
+camara.position.z=5;
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
-document.body.appendChild(renderizador.domElemenet);
-renderizador,render(escena,camera);
+document.body.appendChild(renderizador.domElement);
+renderizador,render(escena,camara);
