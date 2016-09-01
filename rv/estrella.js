9940c9ffcfac1e661201415411d369f65565af5d
chuchulin <chuchulin_99@hotmail.com>
2016-09-01 15:21:42 -0500

    Update estrella.js

--- rv/estrella.js
+++ rv/estrella.js
@@ -1,11 +1,11 @@
 var figura = new THREE.Shape();
 figura.moveTo(-5,8.66);
 figura.lineTo(5,8.66);
-figura.lineTo(8.66,0);
+figura.lineTo(10,0);
 
 figura.lineTo(5,-8.66);
 figura.lineTo(-5,-8.66);
-figura.lineTo(-8.66,0);
+figura.lineTo(-10,0);
 figura.lineTo(-5,8.66);
 var forma = new THREE.ShapeGeometry(figura);
 var malla = new THREE.Mesh(forma);
