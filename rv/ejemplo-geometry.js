da428b429bcdaa6ec087f721c0d03b9505824cf1
chuchulin <chuchulin_99@hotmail.com>
2016-09-01 15:37:23 -0500

    Update ejemplo-geometry.js

--- rv/ejemplo-geometry.js
+++ rv/ejemplo-geometry.js
@@ -7,12 +7,12 @@ forma.vertices.push(new THREE.Vector3(-1,0,-1));
 forma.vertices.push(new THREE.Vector3(-1,0,1));
 forma.vertices.push(new THREE.Vector3(0,1,0));
 //se definen las caras
-forma.faces.push(new THREES.Face3(3,2,1));
-forma.faces.push(new THREES.Face3(3,1,0));
-forma.faces.push(new THREES.Face3(3,0,4));
-forma.faces.push(new THREES.Face3(0,1,4));
-forma.faces.push(new THREES.Face3(1,2,4));
-forma.faces.push(new THREES.Face3(2,3,4));
+forma.faces.push(new THREE.Face3(3,2,1));
+forma.faces.push(new THREE.Face3(3,1,0));
+forma.faces.push(new THREE.Face3(3,0,4));
+forma.faces.push(new THREE.Face3(0,1,4));
+forma.faces.push(new THREE.Face3(1,2,4));
+forma.faces.push(new THREE.Face3(2,3,4));
 ///se hace una esfera de menor tamanio que acota al objeto
 forma.computeBoundingSphere();
 forma.computeFaceNormals();
@@ -23,7 +23,7 @@ var escena = new THREE.Scene();
 escena.add.malla;
 var camara = new THREE.PerspectiveCamera();
 camara.position.z=5;
-var renderizador = new three.WebGLRenderer();
+var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
 renderizador.render(escena,camera);
