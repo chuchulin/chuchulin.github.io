deacf52a96d4ae236ca99f451121e507d65e2ada
chuchulin <chuchulin_99@hotmail.com>
2016-09-06 15:20:05 -0500

    Update ejemplo-camara-orto.js

--- rv/ejemplo-camara-orto.js
+++ rv/ejemplo-camara-orto.js
@@ -6,7 +6,7 @@ camara.bottom=window.innerWidth/-2;
 camara.near=0.1;
 camara.far=1000;
 camara.updateProjectionMatrix();
-camera.position.z=100;
+camara.position.z=100;
 
 var cubo = new THREE.Mesh(new THREE.BoxGeometry(30,30,30),new THREE.MeshNormalMaterial());
 cubo.rotateY(Math.PI/4);
