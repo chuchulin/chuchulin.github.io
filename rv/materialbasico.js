0708e117c699ef636e8d7ff34fc36908c3a2d88f
chuchulin <chuchulin_99@hotmail.com>
2016-09-13 15:31:41 -0500

    Update materialbasico.js

--- rv/materialbasico.js
+++ rv/materialbasico.js
@@ -1,18 +1,24 @@
-/*var luz = new THREE.PointLight(0xFFFFFF);
-luz.position.x=10;
-luz.position.y=10;
-luz.position.z=10;
-*/
+var luz = new THREE.PointLight(0xFFFFFF);
+luz.position.y=2;
+
+
 
 var forma = new THREE.SphereGeometry(1);
-var material    = new THREE.MeshBasicMaterial({color:'0x00FF00'});
+var material    = new THREE.MeshBasicMaterial({color:"#00cc00"});
 var malla    = new THREE.Mesh(forma,material);
+
+var base = new THREE.Mesh(new THREE.BoxGeometry(5,.1,5),new THREE.MeshLambertMaterial({color:0xFFFFFF}));
+
 
 var escena   = new THREE.Scene();
 escena.add(malla);
+escena.add(base);
+escena.add(luz);
+
 
 var camara = new THREE.PerspectiveCamera();
 camara.position.z = 5;
+camara.position.y = 5;
 
 //var lienzo = document.getElementById("LambertMaterial");
 var renderizador = new THREE.WebGLRenderer();
