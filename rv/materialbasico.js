ecd815cc4598c78b649047a50f12f6169fe7dc82
chuchulin <chuchulin_99@hotmail.com>
2016-09-13 15:23:10 -0500

    Update materialbasico.js

--- rv/materialbasico.js
+++ rv/materialbasico.js
@@ -15,7 +15,7 @@ var camara = new THREE.PerspectiveCamera();
 camara.position.z = 5;
 
 //var lienzo = document.getElementById("LambertMaterial");
-var renderizador = new THREE.WebGlRenderer();
-renderizador.setSize(600,600);
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
 renderizador.render(escena,camara);
