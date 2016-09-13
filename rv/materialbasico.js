5f22cef6fc4b0f12afbabfe83bc03522d71f9368
chuchulin <chuchulin_99@hotmail.com>
2016-09-13 15:38:53 -0500

    Update materialbasico.js

--- rv/materialbasico.js
+++ rv/materialbasico.js
@@ -4,7 +4,7 @@ luz.position.y=20;
 
 
 var forma = new THREE.SphereGeometry(1);
-var material    = new THREE.MeshBasicMaterial({color:"#00cc00"});
+var material    = new THREE.MeshLambertMaterial({color:"#00cc00"});
 var malla    = new THREE.Mesh(forma,material);
 malla.position.y=2;
 
