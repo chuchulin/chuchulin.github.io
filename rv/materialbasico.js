50a745a859d04ba263b0be6c39d07f5ebab731a6
chuchulin <chuchulin_99@hotmail.com>
2016-09-20 15:46:25 -0500

    Update materialbasico.js

--- rv/materialbasico.js
+++ rv/materialbasico.js
@@ -1,30 +1,12 @@
 var luz = new THREE.PointLight(0xFFFFFF);
 luz.position.y=20;
 
-var loader = new THREE.ImageLoader();
+var loader = new THREE.TextureLoader();
 
-// load a image resource
+
 loader.load(
-	// resource URL
-	'imagen_047.jpg',
-	// Function when resource is loaded
-	function ( image ) {
-		// do something with it
+	'imagen_047.jpg');
 
-		// like drawing a part of it on a canvas
-		var canvas = document.createElement( 'canvas' );
-		var context = canvas.getContext( '2d' );
-		context.drawImage( image, 100, 100 );
-	},
-	// Function called when download progresses
-	function ( xhr ) {
-		console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
-	},
-	// Function called when download errors
-	function ( xhr ) {
-		console.log( 'An error happened' );
-	}
-);
 
 var forma = new THREE.SphereGeometry(1);
 var material    = new THREE.MeshLambertMaterial({color:"#00cc00"});
