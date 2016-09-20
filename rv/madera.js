61fc7b9c2a3d610ab77139bbd4fbd9058fbe57a7
chuchulin <chuchulin_99@hotmail.com>
2016-09-20 14:49:40 -0500

    Create madera.js

---
+++ rv/madera.js
@@ -0,0 +1,24 @@
+var loader = new THREE.ImageLoader();
+
+// load a image resource
+loader.load(
+	// resource URL
+	'textures/skyboxsun25degtest.png',
+	// Function when resource is loaded
+	function ( image ) {
+		// do something with it
+
+		// like drawing a part of it on a canvas
+		var canvas = document.createElement( 'canvas' );
+		var context = canvas.getContext( '2d' );
+		context.drawImage( image, 100, 100 );
+	},
+	// Function called when download progresses
+	function ( xhr ) {
+		console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+	},
+	// Function called when download errors
+	function ( xhr ) {
+		console.log( 'An error happened' );
+	}
+);
