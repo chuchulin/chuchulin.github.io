0d4158475b219f8ca1d07332ffcb24f04cffbffe
chuchulin <chuchulin_99@hotmail.com>
2016-09-22 15:07:54 -0500

    Update funcion.js

--- rv/funcion.js
+++ rv/funcion.js
@@ -2,7 +2,7 @@ function init(p){
 var malla=new THREE.Mesh(new THREE.BoxGeometry(p,p,p), new THREE.MeshBasicMaterial());
 escenea=new THHREE.Scene();
 escena.add(malla);
-camara=new THREE PerspectiveCamera();
+camara=new THREE.PerspectiveCamera();
 renderizador = new THREE.WEBGLRenderer();
 renderizador.setSize(700,700)
 document.body.appendChild(renderizador.domElement);
