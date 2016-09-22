75fa011d659bfaa55a0664f7538813d2f0dcbcc0
chuchulin <chuchulin_99@hotmail.com>
2016-09-22 15:06:30 -0500

    Create funcion.js

---
+++ rv/funcion.js
@@ -0,0 +1,17 @@
+function init(p){
+var malla=new THREE.Mesh(new THREE.BoxGeometry(p,p,p), new THREE.MeshBasicMaterial());
+escenea=new THHREE.Scene();
+escena.add(malla);
+camara=new THREE PerspectiveCamera();
+renderizador = new THREE.WEBGLRenderer();
+renderizador.setSize(700,700)
+document.body.appendChild(renderizador.domElement);
+}
+var main= funtion(){
+renderizador.render(escena,camara);
+
+
+}
+var escena, camara, renderizador;
+init(1);
+main();
