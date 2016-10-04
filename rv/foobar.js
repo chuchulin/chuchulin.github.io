96a33cad63971c30f7faff74ca4c0880eadf7e1f
chuchulin <chuchulin_99@hotmail.com>
2016-10-04 15:35:11 -0500

    Create foobar.js

---
+++ rv/foobar.js
@@ -0,0 +1,9 @@
+var foo = {nombre: "foo",uno:1 dos:2};
+var bar = {dos:"dos",tres:3},
+
+bar.__proto__=foo;
+console.log(bar.uno);
+console.log(bar.tres);
+console.log(bar.dos);
+console.log(bar.nombre);
+console.log(foo.nombre);
