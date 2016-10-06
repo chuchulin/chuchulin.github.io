216443b4b582a51192aba1b98041cfe504a6b1b1
chuchulin <chuchulin_99@hotmail.com>
2016-10-06 15:05:11 -0500

    Update peon.js

--- rv/peon.js
+++ rv/peon.js
@@ -58,7 +58,7 @@ function init(){
   var toroideBaseMalla = new THREE.Mesh(toroideBaseForma);
 
   // Cuerpo de la torre
-  var cuerpoForma = new THREE.CylinderGeometry(2,4,10,50,50);
+  var cuerpoForma = new THREE.CylinderGeometry(2,4,6,50,50);
   cuerpoForma.translate(0,5,0);
   var cuerpoMalla = new THREE.Mesh(cuerpoForma);
 
